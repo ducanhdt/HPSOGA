f2714df9f09f1e27e517b1f9cc582cd044677a85
Locnqvn <62047642+Locnqvn@users.noreply.github.com>
2020-11-13 17:54:19 +0700

    update

--- framework.py
+++ framework.py
@@ -18,7 +18,9 @@ class Framework():
         self.T=0
         self.btn_flag=0
         self.sit_flag=0
+
         self.solve()
+    
     def read_data_wce(self,path_data_wce):
         '''
         read data of wce
@@ -59,7 +61,7 @@ class Framework():
         '''
         #_,distance=solve_tsp_dynamic_programming(np.array(self.matrix_distance))
         #return distance
-        return 3259.4898121209762
+        return 4270.224625147727
         
     def get_alive(self,path):
         size_path=len(path)
@@ -105,6 +107,7 @@ class Framework():
             L_m=0
             for i in range(n-1):
                 L_m+=self.matrix_distance[path[i]][path[i+1]]
+            L_m+=self.matrix_distance[0][path[0]]+self.matrix_distance[path[n-1]][0]
             return L_m/self.v
         
         def time_charging(path):
@@ -124,12 +127,11 @@ class Framework():
         E_M_tmp=t_tsp_min*self.P_M #minimum moving energy
         E_max_plus_min=self.E_max-self.E_min
         P=0
-        for i in range(1,self.n_sensors):
-            T_i=E_max_plus_min/self.U+E_max_plus_min/(self.U-self.sensors[i].pi)
-            if(self.T<=T_i):
-                self.T=T_i
-            P+=self.sensors[i].pi
-    
+        T_i=[E_max_plus_min/self.U+E_max_plus_min/(self.U-self.sensors[i].pi)
+                for i in range(1,self.n_sensors)]
+        self.T=max(T_i)
+        P=sum([self.sensors[i].pi for i in range(1,self.n_sensors)])
+
         for i in range(1,self.n_sensors):
             t_i_vac=self.T-t_tsp_min-P*self.T/self.U
             if(t_i_vac<0):
@@ -144,3 +146,20 @@ class Framework():
             self.sit_flag=2
         elif(self.btn_flag==1 and self.E_M<E_M_tmp):
             self.sit_flag=3
+        print(self.sit_flag)
+    
+    def compute_path(self,path):
+        n=len(path)
+        dis=0
+        for i in range(n-1):
+            dis+=self.matrix_distance[path[i]][path[i+1]]
+        print(dis)
+
+'''
+path_wce='/home/lnq/Desktop/Hoc/2020/ttth/HPSOGA/wce.txt'
+path_sensor='/home/lnq/Desktop/Hoc/2020/ttth/HPSOGA/sensors.txt'
+path=[0,4,7,19,16,1,12,3,18,15,8,2,6,5,9,13,20,14,11,17,10,0]
+model=Framework(path_wce=path_wce,path_sensor=path_sensor)
+model.solve()
+#model.compute_fitness(path)
+'''
